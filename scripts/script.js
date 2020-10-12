7b5227f5393e7d8d18ff81a12fb7faa4f63f2cab
Kevin <kjh311@gmail.com>
2020-10-12 14:42:55 -0700

    book

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,12 @@ $( document ).ready(function() {
 
 
     $(".dropdown-toggle").dropdown();
-    // $(".navbar-toggler").dropdown();
+
+    $(document).on("click", function(event){
+        // var $trigger = $(".dropdown");
+        // if($trigger !== event.target && !$trigger.has(event.target).length){
+           $(".dropdown-toggle").dropdown();        }     
+
 
 	imgLightbox("img-lightbox-link");
     
