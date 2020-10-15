8054c7c41cbca14595c6b43cca10973cd118006d
Kevin <kjh311@gmail.com>
2020-10-15 11:35:47 -0700

    dropdown

--- scripts/books.js
+++ scripts/books.js
@@ -1,6 +1,8 @@
 $( document ).ready(function() {
-    $(".read-more-button").click(function() {
-        $(".content").slideToggle("slow");
+
+// 01
+    $(".read-more-button01").click(function() {
+        $(".content01").slideToggle("slow");
         var $this = $(this);
         $this.toggleClass("open");
 
@@ -9,9 +11,498 @@ $( document ).ready(function() {
         } else {
             $this.html("Read More");
         }
-        $(".summary").toggle();
+        $(".summary01").toggle();
     });
 
+// 02
+    $(".read-more-button02").click(function() {
+        $(".content02").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary02").toggle();
+    });
+
+    // 03
+    $(".read-more-button03").click(function() {
+        $(".content03").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary03").toggle();
+    });
+
+    // 04
+    $(".read-more-button04").click(function() {
+        $(".content04").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary04").toggle();
+    });
+
+    // 05
+    $(".read-more-button05").click(function() {
+        $(".content05").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary05").toggle();
+    });
+
+    // 06
+    $(".read-more-button06").click(function() {
+        $(".content06").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary06").toggle();
+    });
+
+    // 07
+    $(".read-more-button07").click(function() {
+        $(".content07").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary07").toggle();
+    });
+
+    // 08
+    $(".read-more-button08").click(function() {
+        $(".content08").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary08").toggle();
+    });
+
+    // 09
+    $(".read-more-button09").click(function() {
+        $(".content09").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary09").toggle();
+    });
+
+    // 10
+    $(".read-more-button10").click(function() {
+        $(".content10").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary10").toggle();
+    });
+
+    // 11
+    $(".read-more-button11").click(function() {
+        $(".content11").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary11").toggle();
+    });
+
+    // 12
+    $(".read-more-button12").click(function() {
+        $(".content12").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary12").toggle();
+    });
+
+    // 13
+    $(".read-more-button13").click(function() {
+        $(".content13").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary13").toggle();
+    });
+
+    // 14
+    $(".read-more-button14").click(function() {
+        $(".content14").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary14").toggle();
+    });
+
+    // 15
+    $(".read-more-button15").click(function() {
+        $(".content15").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary15").toggle();
+    });
+
+    // 16
+    $(".read-more-button16").click(function() {
+        $(".content16").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary16").toggle();
+    });
+
+    // 17
+    $(".read-more-button17").click(function() {
+        $(".content17").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary17").toggle();
+    });
+
+    // 18
+    $(".read-more-button18").click(function() {
+        $(".content18").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary18").toggle();
+    });
+
+    // 19
+    $(".read-more-button19").click(function() {
+        $(".content19").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary19").toggle();
+    });
+
+    // 20
+    $(".read-more-button20").click(function() {
+        $(".content20").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary20").toggle();
+    });
+
+    // 21
+    $(".read-more-button21").click(function() {
+        $(".content21").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary21").toggle();
+    });
+
+    // 22
+    $(".read-more-button22").click(function() {
+        $(".content22").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary22").toggle();
+    });
+
+    // 23
+    $(".read-more-button23").click(function() {
+        $(".content23").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary23").toggle();
+    });
+
+    // 24
+    $(".read-more-button24").click(function() {
+        $(".content24").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary24").toggle();
+    });
+
+    // 25
+    $(".read-more-button25").click(function() {
+        $(".content25").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary25").toggle();
+    });
+
+    // 26
+    $(".read-more-button26").click(function() {
+        $(".content26").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary26").toggle();
+    });
+
+    // 27
+    $(".read-more-button27").click(function() {
+        $(".content27").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary27").toggle();
+    });
+
+    // 28
+    $(".read-more-button28").click(function() {
+        $(".content28").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary28").toggle();
+    });
+
+    // 29
+    $(".read-more-button29").click(function() {
+        $(".content29").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary29").toggle();
+    });
+
+    // 30
+    $(".read-more-button30").click(function() {
+        $(".content30").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary30").toggle();
+    });
+
+    // 31
+    $(".read-more-button31").click(function() {
+        $(".content31").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary31").toggle();
+    });
+
+    // 32
+    $(".read-more-button32").click(function() {
+        $(".content32").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary32").toggle();
+    });
+
+    // 33
+    $(".read-more-button33").click(function() {
+        $(".content33").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary33").toggle();
+    });
+
+    // 34
+    $(".read-more-button34").click(function() {
+        $(".content34").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary34").toggle();
+    });
+
+    // 35
+    $(".read-more-button35").click(function() {
+        $(".content35").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary35").toggle();
+    });
+
+    // 36
+    $(".read-more-button36").click(function() {
+        $(".content36").slideToggle("slow");
+        var $this = $(this);
+        $this.toggleClass("open");
+
+        if ($this.hasClass("open")) {
+            $this.html("Read Less");
+        } else {
+            $this.html("Read More");
+        }
+        $(".summary36").toggle();
+    });
 
 
     
