ff2ce742d02fa27fea92660ff525325b7fc8ba3d
Kevin <kjh311@gmail.com>
2020-10-19 16:27:47 -0700

    select links

--- scripts/books.js
+++ scripts/books.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
     $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top + -80
+        scrollTop: $($.attr(this, 'href')).offset().top + -70
     }, 0);
 });
 
