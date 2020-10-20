b13f22ebc12698942067ae0b5f1268bec6012dba
Kevin <kjh311@gmail.com>
2020-10-20 15:48:08 -0700

    dnav

--- scripts/script.js
+++ scripts/script.js
@@ -12,11 +12,11 @@
       $('.nav-dropdown').hide();
     });
     // Toggle open and close nav styles on click
-    $('#nav-toggle').click(function() {
+    $('.nav-toggle').click(function() {
       $('nav ul').slideToggle();
     });
     // Hamburger to X toggle
-    $('#nav-toggle').on('click', function() {
+    $('.nav-toggle').on('click', function() {
       this.classList.toggle('active');
     });
   }); // end DOM ready
