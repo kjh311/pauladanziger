1a243795150a5aa51fe3d4da250aa66a3d3079af
Kevin <kjh311@gmail.com>
2020-10-20 15:21:26 -0700

    dnav

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,23 @@
-$( document ).ready(function() {
-   console.log( "ready!" );
-
-
-
-    $(".dropdown-toggle").dropdown();
-
-    $(document).on("click", function(event){
-        // var $trigger = $(".dropdown");
-        // if($trigger !== event.target && !$trigger.has(event.target).length){
-           $(".dropdown-toggle").dropdown();        }     
-
-
-	imgLightbox("img-lightbox-link");
-    
-});
+(function($) { // Begin jQuery
+  $(function() { // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      // Close one dropdown when selecting another
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    // Clicking away from dropdown will remove the dropdown class
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+    // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    // Hamburger to X toggle
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
