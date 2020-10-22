be35176604f9f3d5e46bd727b1e3c15fdadd1c43
Kevin <kjh311@gmail.com>
2020-10-22 10:32:32 -0700

    dropdown works

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,3 @@
-// (function($) { // Begin jQuery
-//   $(function() { // DOM ready
-//     // If a link has a dropdown, add sub menu toggle.
-//     $('nav ul li a:not(:only-child)').click(function(e) {
-//       $(this).siblings('.nav-dropdown').toggle();
-//       // Close one dropdown when selecting another
-//       $('.nav-dropdown').not($(this).siblings()).hide();
-//       e.stopPropagation();
-//     });
-//     // Clicking away from dropdown will remove the dropdown class
-//     $('html').click(function() {
-//       $('.nav-dropdown').hide();
-//     });
-//     // Toggle open and close nav styles on click
-//     $('.nav-toggle').click(function() {
-//       $('nav ul').slideToggle();
-//     });
-//     // Hamburger to X toggle
-//     $('.nav-toggle').on('click', function() {
-//       this.classList.toggle('active');
-//     });
-
-    
-
-
-//     $('.collapse').collapse()
-//   }); // end DOM ready
-// })(jQuery); // end jQuery
 
 
 
@@ -36,8 +8,11 @@ $( document ).ready(function() {
         event.stopPropagation();
       });
 
+      // $(".navbar-toggler").click(function(){
+      //   $(".navbar-toggler").removeClass('collapsed');
+      // });
+
       $(".collapsed").click(function(){
-        
         $(".collapse1").hide();
         $(".collapse2").hide();
         $(".collapse3").hide();
@@ -46,7 +21,7 @@ $( document ).ready(function() {
         $(".collapse6").hide();
         $(".collapse7").hide();
         $(".collapse8").hide();
-        $('.navbar-collapse').collapse('hide');
+        $('.navbar-collapse').slideToggle();
       });
 
   $(".collapse-button-1").click(function(){
