d4a152d9e2f88d2947868c67cd2636e7056ca735
Kevin <kjh311@gmail.com>
2020-10-22 14:36:49 -0700

    nav all pages

--- scripts/books.js
+++ scripts/books.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
     $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top + -70
+        scrollTop: $($.attr(this, 'href')).offset().top + -50
     }, 0);
 });
 
